bace3bbbebc5fadf07795df9c43c86b6aec98674
Nayoung-Gu <80025366+Nayoung-Gu@users.noreply.github.com>
2022-02-16 20:41:46 +0900

    Update range.js

--- range.js
+++ range.js
@@ -1,10 +1,21 @@
 function solution(array, commands) {
-  let result = [];
-  for (let a = 0; a < commands.length; a++) {
-    let i = commands[a][a];
-    let j = commands[a][a + 1];
-    let k = commands[a][a + 2];
-    result = result.push(array.slice(i, j + 1).sort()[k]);
+  for (let i = 0; i < commands.length; i++) {
+    let result = [];
+    let start = commands[i][0];
+    let end = commands[i][1];
+    let k = commands[i][2];
+    result = result.push(array.slice(start, end + 1).sort()[k + 1]);
     return result;
   }
+}
+
+function solution(array, commands) {
+  let answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    let eachCommand = commands[i];
+    let slice = array.slice(eachCommand[0] - 1, eachCommand[1]);
+    answer.push(slice.sort((a, b) => a - b)[eachCommand[2] - 1]);
+  }
+
+  return answer;
 }
